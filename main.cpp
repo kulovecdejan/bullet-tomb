2af02a770a405891de33077d59f2d23756e98b32
unknown <kulovecdejan>
2025-01-04 23:26:35 +0100

    Add asset folder which contains player sprite and player movement

--- main.cpp
+++ main.cpp
@@ -1,15 +1,32 @@
 #include "raylib.h"
 
-int main() {
-    InitWindow(800, 600, "Hello Raylib");
+int main(void)
+{
+    const int screenWidth = 800;
+    const int screenHeight = 450;
 
-    while (!WindowShouldClose()) {
+    InitWindow(screenWidth, screenHeight, "Bullet Tomb");
+
+    Vector2 ballPosition = { (float)screenWidth/2, (float)screenHeight/2 };
+
+    SetTargetFPS(60);
+
+    while (!WindowShouldClose()) 
+    {
+        if (IsKeyDown(KEY_D)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_A)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_W)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_S)) ballPosition.y += 2.0f;
+
         BeginDrawing();
-        ClearBackground(RAYWHITE);
-        DrawText("Hello, Raylib!", 190, 200, 20, LIGHTGRAY);
+
+            ClearBackground(RAYWHITE);
+            DrawCircleV(ballPosition, 50, MAROON);
+
         EndDrawing();
     }
 
-    CloseWindow();
+   
+    CloseWindow();       
     return 0;
 }
